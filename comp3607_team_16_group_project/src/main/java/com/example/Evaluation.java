0323ef9c095599e7541a3a15f409c56fc0ccd9de
TealTrim <teal.trim20@gmail.com>
2024-11-17 20:52:52 -0400

    Final Code Commit 2

--- comp3607_team_16_group_project/src/main/java/com/example/Evaluation.java
+++ comp3607_team_16_group_project/src/main/java/com/example/Evaluation.java
@@ -1,5 +1,33 @@
-package com.example;
+//-----------------------------
+//Evaluation Interface:
+//-----------------------------
 
-public class Evaluation {
-    
+
+
+
+//-------------------
+//Maven Package Name:
+//-------------------
+package com.example;
+//-------------------
+
+
+
+
+//-------------------
+//Import Statements:
+//-------------------
+import java.io.File;
+//-------------------
+
+
+
+
+//------------------------------------
+//Interface Body:
+//------------------------------------
+public interface Evaluation {
+    int evaluate(File javaFile);
+    String getFeedback();
 }
+//------------------------------------
