a35c7fb83a5c753c5ac37ac647eeb68de7d1b45e
Rian Ramdin <rian-tristen@hotmail.com>
2024-11-17 21:08:46 -0400

    Final Code Commit 3.1

--- comp3607_team_16_group_project/src/main/java/com/example/PDFReportGenerator.java
+++ comp3607_team_16_group_project/src/main/java/com/example/PDFReportGenerator.java
@@ -1,5 +1,5 @@
 //--------------------------------
-//NamingConvention Class:
+//PDFReportGenerator Class:
 //--------------------------------
 
 
@@ -14,69 +14,69 @@ package com.example;
 
 
 
-//------------------
+//------------------------------------------------
 //Import Statements:
-//------------------
+//------------------------------------------------
+import org.apache.pdfbox.pdmodel.*;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import java.io.File;
-//------------------
+import java.io.IOException;
+import java.util.Map;
+//------------------------------------------------
 
 
 
 
-//--------------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------------------
 //Class Body:
-//--------------------------------------------------------------------------------------------------------------------------
-public class NamingConvention implements Evaluation {
-    //----------------------------------------------------------------------------------------------------------------------
-    //Attributes:
-    //----------------------------------------------------------------------------------------------------------------------
-    private String feedback;
-    //----------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------------------
+public class PDFReportGenerator {
+
+    //---------------------------------------------------------------------------------------------------------------------------
+    //Method to generate and format the report:
+    //---------------------------------------------------------------------------------------------------------------------------
+    public void generateReport(Map<File, Map<String, Integer>> results, String studentID, File outputDir) throws IOException {
+        //-----------------------------------------------------------------------------------------------------------------------
+        //Initialize File variable:
+        //-----------------------------------------------------------------------------------------------------------------------
+        File pdfFile = new File(outputDir, studentID + "_Report.pdf");
+        //-----------------------------------------------------------------------------------------------------------------------
 
 
 
 
-    //----------------------------------------------------------------------------------------------------------------------
-    //Methods:
-    //----------------------------------------------------------------------------------------------------------------------
-    //-------------------------------------------------------------------------
-    //Evaluate Method:
-    //-------------------------------------------------------------------------
-    @Override
-    public int evaluate(File javaFile) {
-        //---------------------------------------------------------------------
-        //Initialize Variables:
-        //---------------------------------------------------------------------
-        String fileName = javaFile.getName();
-        //---------------------------------------------------------------------
+        try (PDDocument document = new PDDocument()) {
+            PDPage page = new PDPage();
+            document.addPage(page);
+            try (PDPageContentStream contentStream = new PDPageContentStream(document, page)) {
+                contentStream.setFont(PDType1Font.HELVETICA, 12);
+                contentStream.beginText();
+                contentStream.setLeading(14.5f);
+                contentStream.newLineAtOffset(50, 700);
 
+                contentStream.showText("Evaluation Report for Student: " + studentID);
+                contentStream.newLine();
+                contentStream.newLine();
 
+                for (Map.Entry<File, Map<String, Integer>> entry : results.entrySet()) {
+                    contentStream.showText("File: " + entry.getKey().getName());
+                    contentStream.newLine();
 
+                    int totalScore = 0;
+                    for (Map.Entry<String, Integer> strategyScore : entry.getValue().entrySet()) {
+                        contentStream.showText("  - " + strategyScore.getKey() + ": " + strategyScore.getValue() + " points");
+                        contentStream.newLine();
+                        totalScore += strategyScore.getValue();
+                    }
+                    contentStream.showText("  Total Score: " + totalScore + " points");
+                    contentStream.newLine();
+                    contentStream.newLine();
+                }
 
-        //---------------------------------------------------------------------
-        //Check for proper naming:
-        //---------------------------------------------------------------------
-        if (Character.isUpperCase(fileName.charAt(0))) {
-            feedback = "Naming convention passed.";
-            return 10;
-        } else {
-            feedback = "File name should start with an uppercase letter.";
-            return 5;
+                contentStream.endText();
+            }
+            document.save(pdfFile);
         }
-        //---------------------------------------------------------------------
     }
-    //-------------------------------------------------------------------------
-
-
-
-
-    //-------------------------------------------------------------------------
-    //Getter Methods:
-    //-------------------------------------------------------------------------
-    @Override
-    public String getFeedback() {
-        return feedback;
-    }
-    //-------------------------------------------------------------------------
 }
-//--------------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------------------
